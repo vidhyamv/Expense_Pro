672de69f2401e43af01bc0ca5aa1b2a72f2e06b3
vidhya.m.v <vidhya.m.v@panapps.co>
2026-06-18 23:40:06 +0530

    fix: set ALLOWED_HOSTS default to *

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
 
 # Application definition
